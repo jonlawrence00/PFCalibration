59bef37cd6fb02de8a37ba2352619c9bdf988dc3
jonlawrence00 <118470214+jonlawrence00@users.noreply.github.com>
2024-09-15 21:44:43 -0500

    Update DynamicReductionNetworkJit.py

--- DRN_for_PF/models/DynamicReductionNetworkJit.py
+++ DRN_for_PF/models/DynamicReductionNetworkJit.py
@@ -6,10 +6,10 @@ import numpy as np
 import torch
 import gc
 import torch.nn as nn
+import torch.nn.init as init
 from torch.nn.functional import softplus
 import torch_geometric.transforms as T
 
-#test
 from torch.utils.checkpoint import checkpoint
 from torch_cluster import knn_graph, graclus_cluster
 from torch_scatter import scatter
@@ -66,6 +66,7 @@ def pool_edge(cluster, edge_index, edge_attr: Optional[torch.Tensor] = None):
 
 def _aggr_pool_x(cluster, x, aggr: str, size: Optional[int] = None):
     """Call into scatter with configurable reduction op"""
+    #print("aggr: ", aggr)
     return scatter(x, cluster, dim=0, dim_size=size, reduce=aggr)
 
 def global_pool_aggr(x, batch: OptTensor, aggr: str, size: Optional[int] = None):
@@ -81,7 +82,10 @@ def global_pool_aggr(x, batch: OptTensor, aggr: str, size: Optional[int] = None)
 # in particular edge_attr can be removed since it is always None
 def aggr_pool(cluster, x, batch: OptTensor, aggr: str) -> Tuple[Tensor, OptTensor]:
     """jit-friendly version of max/mean/add pool"""
+    #print("cluster before; ", len(cluster))
     cluster, perm = consecutive_cluster(cluster)
+    #print("cluster after; ", len(cluster))
+    #print("perm: ,", len(perm))
     x = _aggr_pool_x(cluster, x, aggr)
     if batch is not None:
         batch = pool_batch(perm, batch)
@@ -165,6 +169,80 @@ class DynamicReductionNetworkJit(nn.Module):
 
         #construct inputnet
         in_layers_l = []
+        temp_layer = nn.Linear(input_dim, hidden_dim)
+        init.kaiming_uniform_(temp_layer.weight)
+        in_layers_l += [temp_layer,nn.ReLU()]
+
+        for i in range(in_layers-1):
+            temp_layer = nn.Linear(hidden_dim, hidden_dim)
+            init.kaiming_uniform_(temp_layer.weight)
+            in_layers_l += [temp_layer, 
+                    nn.ReLU()]
+
+        self.inputnet = nn.Sequential(*in_layers_l)
+        #print("hi there big dog")
+        #print("input net: ", self.inputnet)
+        
+        #construct aggregation layers
+        self.agg_layers = nn.ModuleList()
+        print("agg layers: ", self.agg_layers)
+        for i in range(agg_layers):
+            e = 2**(i+1)
+            #print("hello there mate 1")
+            #construct message passing network
+            mp_layers_l = []
+
+            for j in range(mp_layers-1):
+                #print("hello there mate 2, He init is done")
+                temp_layer = nn.Linear(2*hidden_dim, 2*hidden_dim)
+                init.kaiming_uniform_(temp_layer.weight)
+                mp_layers_l += [temp_layer, nn.BatchNorm1d(2*hidden_dim), nn.ReLU()]
+
+            temp_layer = nn.Linear(2*hidden_dim, hidden_dim)
+            init.kaiming_uniform_(temp_layer.weight)
+            mp_layers_l += [temp_layer, nn.BatchNorm1d(hidden_dim), nn.ReLU()]
+           
+            convnn = nn.Sequential(*mp_layers_l)
+            
+            self.agg_layers.append(EdgeConv(nn=convnn, aggr=aggr).jittable())
+            #print("aggr: ", aggr)
+            #print("convnn: " , convnn)
+
+        print("agg layers: ", self.agg_layers)
+        #print("mp layers: ",mp_layers)
+        
+        #construct outputnet
+        out_layers_l = []
+
+        #temp_layer = nn.Linear(4*hidden_dim+self.graph_features, 2*hidden_dim+self.graph_features)
+        #init.kaiming_uniform_(temp_layer.weight)
+        #out_layers_l += [temp_layer, nn.BatchNorm1d(4*hidden_dim), nn.ReLU(), nn.Dropout(.1)]
+
+        #temp_layer = nn.Linear(2*hidden_dim+self.graph_features, hidden_dim+self.graph_features)
+        #init.kaiming_uniform_(temp_layer.weight)
+        #out_layers_l += [temp_layer, nn.BatchNorm1d(1), nn.ReLU()]
+
+        for i in range(out_layers-1):
+            temp_layer = nn.Linear(hidden_dim+self.graph_features, hidden_dim+self.graph_features)
+            init.kaiming_uniform_(temp_layer.weight)
+            out_layers_l += [temp_layer, nn.ReLU(), nn.Dropout(.1)]
+
+
+        temp_layer = nn.Linear(hidden_dim+self.graph_features, output_dim)
+        init.kaiming_uniform_(temp_layer.weight)
+        out_layers_l += [temp_layer]
+
+        self.output = nn.Sequential(*out_layers_l)
+
+        if pool not in {'max', 'mean', 'add'}:
+            raise Exception("ERROR: INVALID POOLING")
+        
+        self.aggr_type = pool
+
+        print("output: ", self.output)
+
+        '''#construct inputnet
+        in_layers_l = []
         in_layers_l += [nn.Linear(input_dim, hidden_dim),
                 nn.ELU()]
 
@@ -174,16 +252,19 @@ class DynamicReductionNetworkJit(nn.Module):
 
         self.inputnet = nn.Sequential(*in_layers_l)
 
-
+        print("input net: ", self.inputnet)
+        
         #construct aggregation layers
         self.agg_layers = nn.ModuleList()
+        print("agg layers: ", self.agg_layers)
         for i in range(agg_layers):
+            print("hello there mate 1")
             #construct message passing network
             mp_layers_l = []
 
             for j in range(mp_layers-1):
-                mp_layers_l += [nn.Linear(2*hidden_dim, 2*hidden_dim),
-                        nn.ELU()]
+                print("hello there mate 2")
+                mp_layers_l += [nn.Linear(2*hidden_dim, 2*hidden_dim), nn.ELU()]
 
             mp_layers_l += [nn.Linear(2*hidden_dim, hidden_dim),
                     nn.ELU()]
@@ -191,13 +272,18 @@ class DynamicReductionNetworkJit(nn.Module):
             convnn = nn.Sequential(*mp_layers_l)
             
             self.agg_layers.append(EdgeConv(nn=convnn, aggr=aggr).jittable())
+            print("aggr: ", aggr)
+            print("convnn: " , convnn)
 
+        print("agg layers: ", self.agg_layers)
+        #print("mp layers: ",mp_layers)
+        
         #construct outputnet
         out_layers_l = []
 
         for i in range(out_layers-1):
-            out_layers_l += [nn.Linear(hidden_dim+self.graph_features, hidden_dim+self.graph_features), 
-                    nn.ELU()]
+            out_layers_l += [nn.Linear(hidden_dim+self.graph_features, hidden_dim+self.graph_features), nn.ELU()]
+            #out_layers_l += [nn.Linear(hidden_dim+self.graph_features, hidden_dim+self.graph_features), nn.ELU(), nn.Dropout(.1)]
 
         out_layers_l += [nn.Linear(hidden_dim+self.graph_features, output_dim)]
 
@@ -208,47 +294,150 @@ class DynamicReductionNetworkJit(nn.Module):
         
         self.aggr_type = pool
 
+        print("output: ", self.output)'''
+
+        '''#construct inputnet
+        in_layers_l = []
+        in_layers_l += [nn.Linear(input_dim, hidden_dim),
+                nn.ELU()]
+
+        for i in range(in_layers-1):
+            in_layers_l += [nn.Linear(hidden_dim, hidden_dim), 
+                    nn.ELU()]
+
+        self.inputnet = nn.Sequential(*in_layers_l)
+
+        print("input net: ", self.inputnet)
+        
+        #construct aggregation layers
+        self.agg_layers = nn.ModuleList()
+        print("agg layers: ", self.agg_layers)
+        for i in range(agg_layers):
+            e = 2**(i+1)
+            print("hello there mate 1")
+            #construct message passing network
+            mp_layers_l = []
+
+            for j in range(mp_layers-1):
+                print("hello there mate 2")
+                mp_layers_l += [nn.Linear(e*hidden_dim, e*hidden_dim), nn.ELU()]
+
+            #mp_layers_l += [nn.Linear(2*hidden_dim, hidden_dim),nn.ELU()]
+           
+            convnn = nn.Sequential(*mp_layers_l)
+            
+            self.agg_layers.append(EdgeConv(nn=convnn, aggr=aggr).jittable())
+            print("aggr: ", aggr)
+            print("convnn: " , convnn)
+
+        print("agg layers: ", self.agg_layers)
+        #print("mp layers: ",mp_layers)
+        
+        #construct outputnet
+        out_layers_l = []
+
+        out_layers_l += [nn.Linear(4*hidden_dim+self.graph_features, 2*hidden_dim+self.graph_features), nn.ELU()]
+
+        out_layers_l += [nn.Linear(2*hidden_dim+self.graph_features, hidden_dim+self.graph_features), nn.ELU()]
+        
+        for i in range(out_layers-1):
+            out_layers_l += [nn.Linear(hidden_dim+self.graph_features, hidden_dim+self.graph_features), nn.ELU()]
+            #out_layers_l += [nn.Linear(hidden_dim+self.graph_features, hidden_dim+self.graph_features), nn.ELU(), nn.Dropout(.1)]
+
+        out_layers_l += [nn.Linear(hidden_dim+self.graph_features, output_dim)]
+
+        self.output = nn.Sequential(*out_layers_l)
+
+        if pool not in {'max', 'mean', 'add'}:
+            raise Exception("ERROR: INVALID POOLING")
+        
+        self.aggr_type = pool
+
+        print("output: ", self.output)'''
+
     def forward(self, x: Tensor, batch: OptTensor, graph_x: OptTensor) -> Tensor:
         '''
         Push the batch 'data' through the network
         '''
+        #print("################################")
+        #print("initial size of x: ", x.size())
+        #print("initial x: ",len(x))
         x = self.datanorm * x
+        #print("self.datanorm: ", self.datanorm)
         x = self.inputnet(x)
-
+        #print("input x", len(x))
+        #print("initial size of x: ", x.size())
         latent_probe = self.latent_probe
         
         if graph_x is not None:
+            #print("graph is none")
             graph_x = graph_x.view((-1, self.graph_features))
 
         # if there are no aggregation layers just leave x, batch alone
         nAgg = len(self.agg_layers)
+        #counter = 0
         for i, edgeconv in enumerate(self.agg_layers):
+            initial = x
+            #counter += 1
             if latent_probe is not None and i == latent_probe:
                 return x
+            #print("batch len", len(batch))
+            #print("size of batch: ", batch.size())
+            #print(batch)
             knn = knn_graph(x, self.k, batch, loop=self.loop, flow=edgeconv.flow)
+            #print("knn : " ,knn)
+            #print("self.kL " , len(self.k))
+            #print(self.k)
+            #print("len of x:", len(x))
+            #print("knn: ", knn[0].shape())
             edge_index = to_undirected(knn)
+            #print("#i: ", i, " #edgeconv: ", edgeconv)
+            #print("x[0] before edgeconv: ", x[0])
+            #print("x.size before edgeconv: ", x.size())
             x = edgeconv(x, edge_index)
+            #print("x[0] after edgeconv: ", x[0])
+            x+=initial
+            #print("x.size after edgeconv: ", x.size())
+            #print("edge index: ", edge_index)
+
 
             weight = normalized_cut_2d(edge_index, x)
+            #print("weight: ", weight.size())
             cluster = graclus_cluster(edge_index[0], edge_index[1], weight, x.size(0))
+            #print("cluster: ", cluster.size())
+            #print(cluster[0:100])
 
+            #print("batch: ", batch)
             if i == nAgg - 1:
                 x, batch = aggr_pool_x(cluster, x, batch, self.aggr_type)
             else:
+                #print("hello?")
                 x, batch = aggr_pool(cluster, x, batch, self.aggr_type)
 
+            #print("batch: ", batch[0:10])
+            #print("x.size after aggr_pool: ", x.size())
+            #print("x[0] after aggregate: ", x[0])
+            #print("batch.size after aggr_pool: ", batch)
+
         if latent_probe is not None and latent_probe == nAgg:
+            #print("latent is none")
             return x
 
         # this xforms to batch-per-row so no need to return batch
+        #print("after agg x.size", x.size())
         x = global_pool_aggr(x, batch, self.aggr_type)
+        #print("after global x.size", x.size())
 
         if latent_probe is not None and latent_probe == nAgg + 1:
+            #print("latent is none")
             return x
 
+        #print("size of graph_x; ", graph_x)
         if graph_x is not None:
+            #print("graph is not none 2")
             x = torch.cat((x, graph_x), 1)
 
         x = self.output(x).squeeze(-1)
-
+        #print("size of x at the end: ", x.size())
+        #print("####################################################")
         return x
